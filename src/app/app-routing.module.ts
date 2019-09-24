4f3b96ab23e1e0a2d28fcddecd12833b35c7eb2f
OKondratenko_UDT <Oleksandr.Kondratenko@upc.ua>
2019-09-24 16:57:29 +0300

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import {MarketComponent} from './market/market.component';
+import {TradersComponent} from './traders/traders.component';
+import {CommonModule} from '@angular/common';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/market', pathMatch: 'full'},
+  {path: 'traders', component: TradersComponent},
+  {path: 'market', component: MarketComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
