9ad1784cf74188190e8f709001640b8984d268be
OKondratenko_UDT <Oleksandr.Kondratenko@upc.ua>
2019-09-24 15:28:34 +0300

    Stock

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Stock} from './model/Stock';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'trading-app';
+
+  stocks: Stock[];
+
+  ngOnInit(): void {
+    this.stocks = this.getMockStocks();
+  }
+
+  private getMockStocks(): Stock[] {
+    let stocks: Stock[] = [];
+
+    stocks.push(new Stock('BA', 'Boeing'));
+    stocks.push(new Stock('CA', 'CAT'));
+    stocks.push(new Stock('CO', 'Coca-Cola'));
+
+    return stocks;
+  }
 }
